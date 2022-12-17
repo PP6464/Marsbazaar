fbf900ff58173e7fad534ba74bc2bb5955f2db22
Panth Patel <panthhp@outlook.com>
2022-12-17 13:36:04 +0000

    utils

---
+++ lib/util/colours.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+extension ColourExt on theme {
+  Color get colour {
+    switch (this) {
+      case theme.teal:
+        return const Color(0xFF18AD84);
+      case theme.darkBlue:
+        return const Color(0xFF242B69);
+      case theme.black:
+        return Colors.black;
+      case theme.white:
+        return Colors.white;
+      case theme.red:
+        return const Color(0xFFF53E5E);
+    }
+  }
+}
+
+// ignore: camel_case_types
+enum theme {
+  teal,
+  darkBlue,
+  white,
+  black,
+  red
+}
---
+++ lib/util/elements.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget logoExpanded = Image.asset("assets/logo_expanded.png");
+Widget logoExpandedBig = Image.asset("assets/logo_expanded_big.png");
+Widget loginOverlayBR = Image.asset("assets/login_overlay_br.png");
+Widget loginOverlayTR = Image.asset("assets/login_overlay_tr.png");
+Widget loginOnboard = Image.asset("assets/login_onboard.png");
