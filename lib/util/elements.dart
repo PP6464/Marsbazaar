11ed0764d9f69a46b05a8eda24158bc8297c6181
Panth Patel <panthhp@outlook.com>
2022-12-18 21:54:20 +0000

    elements

--- lib/util/elements.dart
+++ lib/util/elements.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:marsbazaar/util/theme.dart';
 
 Widget logoExpanded = Image.asset("assets/logo_expanded.png");
 Widget logoExpandedBig = Image.asset("assets/logo_expanded_big.png");
 Widget loginOverlayBR = Image.asset("assets/login_overlay_br.png");
 Widget loginOverlayTR = Image.asset("assets/login_overlay_tr.png");
 Widget loginOnboard = Image.asset("assets/login_onboard.png");
+const Widget blank = SizedBox(width: 0.0, height: 0.0);
+Text errorText(BuildContext context) => Text(AppLocalizations.of(context)!.error);
+Widget loadingIndicator = CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(theme.teal.colour), backgroundColor: Colors.teal[700]!,);
