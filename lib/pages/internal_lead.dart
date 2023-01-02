44de8e556b721755c59aa0083eb125f01f4ffd32
Panth Patel <panthhp@outlook.com>
2023-01-02 11:58:23 +0000

    internal lead

---
+++ lib/pages/internal_lead.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class InternalLeadPage extends StatefulWidget {
+  const InternalLeadPage({Key? key}) : super(key: key);
+
+  @override
+  State<InternalLeadPage> createState() => _InternalLeadPageState();
+}
+
+class _InternalLeadPageState extends State<InternalLeadPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
