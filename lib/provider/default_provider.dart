0e11322decd95436c6bb655751ace04555704a91
Panth Patel <panthhp@outlook.com>
2023-01-05 18:54:18 +0000

    default provider

--- lib/provider/default_provider.dart
+++ lib/provider/default_provider.dart
@@ -4,3 +4,6 @@ import 'package:provider/provider.dart';
 
 AppState defaultProvider(BuildContext context, [bool listen = false]) => Provider.of<AppState>(context, listen: listen);
 String defaultUserName = "Dhruv";
+String defaultUserEmail = "dhruvgohel@marsbazaar.com";
+String defaultUserPhotoURL = "https://eu.ui-avatars.com/api/?background=18AD84&size=128&name=Dhruv+Gohel&color=FFFFFF&rounded=true&bold=true";
+bool defaultUserAdmin = true;
